674292b2f2c0e8eabc6dae1eeeaea237dbaac337
YASHRAJ POWAR <yashpowar529@gmail.com>
2025-11-11 15:25:55 +0530

    Delete Data directory

--- Data/30HR DAILY aggrigatrd data/ADVANCE techniques/EXECUTION[99991].sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-*
-FROM FactResellerSales
-WHERE CarrierTrackingNumber = '4911-403C-98'
-
-CREATE NONCLUSTERED INDEX IND_factResellerSales_CTA 
-ON FactResellerSales (CarrierTrackingNumber)
--- Data/30HR DAILY aggrigatrd data/SQLQuery5[3].sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO customers (id, first_name,country,score)
-VALUES
-(7, 'YASTH', 'INDHIA', 100)
